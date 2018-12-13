e48d697a25ecb63d8b5100e5f3112d74cab127dc
Daniil Sloboda <dsloboda@cloudlinux.com>
2018-12-13 12:27:01 +0300

    find the bearing element and report the solution function

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,12 @@ public class Main {
             4
     };
 
-    private static int[] freeX; // numbers of the Xes, which are free
-    private static int[] basisX;
-    private static double[][] arr;
+    private static int[] freeX = new int[c.length]; // numbers of the Xes, which are free
+    private static int[] basisX = new int[b.length];
+    private static double[][] arr = new double [b.length + 1][c.length + 1];
+
+    private static int permittingColumn; // they are the indexes in arr!
+    private static int permittingRow;
 
     /*
      * Table structure:
@@ -35,13 +38,12 @@ public class Main {
      *           Q          = .. + ........ + ........ + ........ + ...................
      *
      * x1 - x5 are free now, x6 - x8 are basis (is is just an example, it depends on b.length and c.length)
-     * they are in the freeX and basisX arrays (the order is important!)
+     * They are in the freeX and basisX arrays (the order is important!)
+     *
+     * There will be b.length of basis vars and c.length of free vars and also 1 not-x number.
+     * The last line contains coefficients for the goal function.
      */
     private static void initTable() {
-        // there will be b.length of basis vars and c.length of free vars and also 1 nor-x number.
-        // The last line contains coefficients for the goal function.
-        arr = new double [b.length + 1][c.length + 1];
-
         // at the beginning we have additional vars at the basis, others are free
         // first (№0) column contains free numbers (in the beginning they are b)
         for (int i = 0; i < b.length; i++) {
@@ -71,19 +73,59 @@ public class Main {
 
     private static boolean isTheGoalMinimum() {
         boolean isMinimum = true;
-        for(int j = 1; j < c.length + 1; j++) {
+        for (int j = 1; j < c.length + 1; j++) {
             isMinimum = isMinimum && arr[b.length][j] >= 0;
         }
         return isMinimum;
     }
 
+    private static void findTheBearingElement() {
+        // find the permittingColumn
+        double minAtQ = arr[b.length][1];
+        permittingColumn = 1;
+        for (int i = 2; i < c.length + 1; i++) {
+            if (arr[b.length][i] < minAtQ) {
+                minAtQ = arr[b.length][i];
+                permittingColumn = i;
+            }
+        }
 
+        // find the permitting row
+        double minAtPermittingColumn = Double.MAX_VALUE;
+        for (int i = 0; i < b.length; i++) {
+            double tmp = arr[i][0] / arr[i][permittingColumn];
+            if (arr[i][permittingColumn] < 0 && tmp < minAtPermittingColumn) {
+                minAtPermittingColumn = tmp;
+                permittingRow = i;
+            }
+        }
+
+        // if there were no elements < 0 in the column
+        if (minAtPermittingColumn == Double.MAX_VALUE) {
+            System.out.println("Oh, sorry, it seems like there is no solution at all");
+            System.exit(0);
+        }
+    }
+
+    private static void reportTheSolution() {
+        System.out.println("I have found the solution!");
+        System.out.println("The goal function is " + arr[b.length][0]);
+        System.out.println("At:");
+        for(int i = 0; i < freeX.length; i++) {
+            System.out.println("x" + freeX[i] + " = 0");
+        }
+        for(int i = 0; i < basisX.length; i++) {
+            System.out.println("x" + basisX[i] + " = " + arr[i][0]);
+        }
+    }
 
     public static void main(String[] args) {
         initTable();
 
         while(!isTheGoalMinimum()) {
-
+            findTheBearingElement();
+            // TODO
         }
+        reportTheSolution();
     }
 }
